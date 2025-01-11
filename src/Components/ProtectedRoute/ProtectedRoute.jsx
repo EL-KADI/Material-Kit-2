eeeacd0ac89b4a9c44f62045d0f23cd91a9cc23e
Start-Framework <sayedragheb92@gmail.com>
2025-01-11 03:01:58 +0200

    Material Kit 2

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,22 +1,27 @@
 import PropTypes from "prop-types";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import Cookies from "js-cookie";
 
 const ProtectedRoute = ({ children }) => {
   const location = useLocation();
   const user = Cookies.get("user");
 
-
-  if (user && (location.pathname === "/login" || location.pathname === "/register")) {
-    return <div className="d-none">{children}</div>;
+  if (
+    user &&
+    (location.pathname === "/login" || location.pathname === "/register")
+  ) {
+    return <Navigate to="/" />;
   }
 
-  if (!user && location.pathname !== "/login" && location.pathname !== "/register") {
-    return <div className="d-none">{children}</div>;
+  if (
+    !user &&
+    location.pathname !== "/login" &&
+    location.pathname !== "/register"
+  ) {
+    return <Navigate to="/register" />;
   }
 
-
-  return <div>{children}</div>;
+  return children;
 };
 
 ProtectedRoute.propTypes = {
